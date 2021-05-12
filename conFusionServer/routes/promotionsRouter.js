b47f9fef5de741cc7b49fe559c9a29d634b4620d
zunaia119 <zunaira0107@gmail.com>
2021-05-12 22:39:35 +0500

    express generator rest api's

---
+++ conFusionServer/routes/promotionsRouter.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const promotionsRouter = express.Router();
+
+promotionsRouter.use(bodyParser.json());
+
+promotionsRouter.route('/:promoId')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send all the promotins to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the promotion: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /promotion');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all promotions');
+});
+
+module.exports = promotionsRouter;
