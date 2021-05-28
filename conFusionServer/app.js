db41e5c7f29adc6d5efff6ef82c5183fe9072a0d
zunaia119 <zunaira0107@gmail.com>
2021-05-28 13:45:53 +0500

    Cookies

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -25,28 +25,40 @@ connect.then((db) => {
 }, (err) => { console.log(err); });
 
 var app = express();
+app.use(cookieParser('12345-67890-09876-54321'));
 function auth (req, res, next) {
-  console.log(req.headers);
+if (!req.signedCookies.user) {
   var authHeader = req.headers.authorization;
   if (!authHeader) {
       var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
+      res.setHeader('WWW-Authenticate', 'Basic');              
       err.status = 401;
       next(err);
       return;
   }
-
   var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
   var user = auth[0];
   var pass = auth[1];
   if (user == 'admin' && pass == 'password') {
+      res.cookie('user','admin',{signed: true});
       next(); // authorized
   } else {
       var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');      
+      res.setHeader('WWW-Authenticate', 'Basic');              
       err.status = 401;
       next(err);
   }
+}
+else {
+    if (req.signedCookies.user === 'admin') {
+        next();
+    }
+    else {
+        var err = new Error('You are not authenticated!');
+        err.status = 401;
+        next(err);
+    }
+}
 }
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
