20a1d9b24e42cb9e30a63ad487173345b1d103ca
zunaia119 <zunaira0107@gmail.com>
2021-05-31 11:44:25 +0500

    passport

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -8,7 +8,8 @@ const Dishes = require('./models/dishes');
 const Promotions = require('./models/promotions');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
-
+var passport = require('passport');
+var authenticate = require('./authenticate');
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 var app = express();
@@ -67,8 +68,7 @@ connect.then((db) => {
 //         }
 //     }
 // }
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use(cookieParser('12345-67890-09876-54321'));
 app.use(session({
   name: 'session-id',
@@ -77,23 +77,21 @@ app.use(session({
   resave: false,
   store: new FileStore()
 }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 function auth (req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-if(!req.session.user) {
+if(!req.user) {
     var err = new Error('You are not authenticated!');
     err.status = 403;
     return next(err);
 }
 else {
-  if (req.session.user === 'authenticated') {
+  
     next();
-  }
-  else {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    return next(err);
-  }
 }
 }
 // view engine setup
